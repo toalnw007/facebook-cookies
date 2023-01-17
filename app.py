79419cd88b393b9f1b3bffdcd833d104dd9951f0
DeltaDevTH <54938209+vitsarutnan@users.noreply.github.com>
2023-01-18 00:48:50 +0700

    Update def api_gen_list_token

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, jsonify, url_for, redirect, session, flash, g
+from flask import Flask, render_template, request, jsonify, url_for, redirect, session, flash, g, send_file
 from datetime import date
 import json
 import requests,re
@@ -8,14 +8,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return "Hello, Flask!"
 
 @app.route('/gen-list-token', methods=['POST'])
 def api_gen_list_token():
     data_json = request.json
     list_cookies = data_json['list_cookies']
     list_token = []
-    for x in list_cookies :
+
+    f = open("listToken.txt", "a")
+    
+    for cookie in list_cookies :
         data = requests.get('https://business.facebook.com/business_locations', headers = {
             'user-agent'                : 'Mozilla/5.0 (Linux; Android 8.1.0; MI 8 Build/OPM1.171019.011) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.86 Mobile Safari/537.36', # don't change this user agent.
             'referer'                   : 'https://www.facebook.com/',
@@ -27,13 +30,24 @@ def api_gen_list_token():
             'accept'                    : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
             'content-type'              : 'text/html; charset=utf-8'
         }, cookies = {
-            'cookie'                    : x
+            'cookie'                    : cookie
         })
 
         find_token = re.search('(EAAG\w+)', data.text)
-        list_token.append({"cookies":x,"token":find_token.group(1)})
 
-    return jsonify(list_token)
+        token = ''
+        if not(find_token is None):
+            token = find_token.group(1)
+
+        text = "Cookie {cookie} | Token {token}".format(cookie = cookie, token = token)
+        f.write(text)
+        f.write('\n')
+
+        list_token.append({"Cookie": cookie, "Token": token})
+
+    f.close()
+    path = "listToken.txt"
+    return send_file(path, as_attachment=True)
 
 
 if __name__ == '__main__':
