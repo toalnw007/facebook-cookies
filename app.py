185eadc03e4402fb080f470688e646e4feb890c4
Sunteen <phitthawat.m@sunteen.co.th>
2023-01-18 00:56:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return render_template('index.html')
 
 @app.route('/gen-list-token', methods=['POST'])
 def api_gen_list_token():
